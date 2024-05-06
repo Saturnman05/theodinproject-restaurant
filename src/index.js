f772220313c28f442cb857c415e4ae53cebac9ca
francisco <franciscomaderanatear@gmail.com>
2024-05-06 16:29:01 -0400

    template and css working how i want

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import Menu from './Menu';
 import About from './About';
 import './style.css';
 
-const h1 = document.createElement('h1');
-h1.innerText = 'Restaurant';
-document.body.appendChild(h1);
-
-const content = document.createElement('div');
-content.id = 'content';
-document.body.appendChild(content);
+Home();
 
 const btnHome = document.querySelector('#home');
 btnHome.addEventListener('click', () => {
