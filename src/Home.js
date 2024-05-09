a40e3a5414a1653e3af394a0f0197d719bf92091
francisco <franciscomaderanatear@gmail.com>
2024-05-09 09:47:11 -0400

    Menu page completed

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,24 @@
+function createParagraph(text) {
+    const paragraph = document.createElement('p');
+    paragraph.innerText = text;
+    return paragraph;
+}
+
 function Home() {
     const content = document.querySelector('#content');
     content.innerHTML = '';
 
-    const firstParagraph = document.createElement('p');
-    firstParagraph.innerText = 'Welcome to the restaurant.';
-
+    const firstParagraph = createParagraph('Welcome to Pizza Mania Restaurnant.');
     content.appendChild(firstParagraph);
+
+    const secondParagraph = createParagraph('Discover the authentic taste of Italy in every bite. At Delicious Flavor Pizzeria, we strive to bring you the freshest and most flavorful pizzas, prepared with the highest quality ingredients and cooked to perfection in our traditional stone oven. From our classic pepperoni pizzas to our unique gourmet creations, each slice is a culinary experience that will transport you to the streets of Naples.');
+    content.appendChild(secondParagraph);
+
+    const thirdParagraph = createParagraph('At Delicious Flavor Pizzeria, customer satisfaction is our top priority. From the moment you walk through the door to the last bite of your pizza, we strive to provide exceptional service and a memorable dining experience.');
+    content.appendChild(thirdParagraph);
+
+    const fourthParagraph = createParagraph('Come and join us today to enjoy an authentic Italian pizza experience!');
+    content.appendChild(fourthParagraph);
 }
 
 export default Home;
--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,71 @@
+function createUl(...texts) {
+    const ul = document.createElement('ul');
+    for (const text of texts) {
+        const li = document.createElement('li');
+        li.innerText = text;
+        ul.appendChild(li);
+    }
+    return ul;
+}
+
+function createMenu(h2Text, ...texts) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.innerText = h2Text;
+    div.appendChild(h2);
+
+    const ul = createUl(...texts);
+    div.appendChild(ul);
+
+    return div;
+}
+
 function Menu() {
     const content = document.querySelector('#content');
     content.innerHTML = '';
 
-    const firstParagraph = document.createElement('p');
-    firstParagraph.innerText = 'Welcome to the menu of the restaurant.';
+    const h2 = document.createElement('h2');
+    h2.innerText = 'Menu:';
+    content.appendChild(h2);
 
-    content.appendChild(firstParagraph);
+    const div1 = createMenu(
+        'Classic Pizzas',
+        'Margherita: Tomato sauce, fresh mozzarella, fresh basil.', 
+        'Pepperoni: Tomato sauce, mozzarella, pepperoni.', 
+        'Hawaiian: Tomato sauce, mozzarella, ham, pineapple.',
+    )
+    content.appendChild(div1);
+
+    const div2 = createMenu(
+        'Gourmet Pizzas',
+        'Prosciutto and Arugula: Tomato sauce, mozzarella, prosciutto, arugula, parmesan cheese.',
+        'Four Cheese: Mozzarella, blue cheese, goat cheese, parmesan cheese, white sauce.',
+        'Vegetarian: Tomato sauce, mozzarella, mushrooms, bell peppers, red onion, black olives, artichokes.',
+    );
+    content.appendChild(div2);
+
+    const div3 = createMenu(
+        'Appetizers',
+        'Garlic Breadsticks with Marinara Sauce.',
+        'Tomato and Basil Bruschetta.',
+        'Fried Calamari with Lemon Aioli.',
+    );
+    content.appendChild(div3);
+
+    const div4 = createMenu(
+        'Salads',
+        'Traditional Caesar: Romaine lettuce, croutons, parmesan cheese, Caesar dressing.',
+        'Mediterranean: Mixed greens, cherry tomatoes, cucumber, kalamata olives, feta cheese, herb vinaigrette dressing.',
+    );
+    content.appendChild(div4);
+
+    const div5 = createMenu(
+        'Desserts',
+        'Homemade Tiramisu.',
+        'Ricotta and Chocolate-filled Cannoli.',
+        'Italian Gelato (Variety of flavors available).',
+    )
+    content.appendChild(div5);
 }
 
 export default Menu;
