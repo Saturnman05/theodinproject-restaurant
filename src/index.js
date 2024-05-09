a33c764440a509a2d80bf6089457a065efbcd6d1
francisco <franciscomaderanatear@gmail.com>
2024-05-09 13:50:40 -0400

    Image added and page finished

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import Home from './Home';
 import Menu from './Menu';
 import About from './About';
 import './style.css';
+import Icon from './pizzaimg.jpg';
 
+// Poner la imagen
+const img = document.querySelector('#img');
+const myIcon = new Image();
+myIcon.src = Icon;
+myIcon.alt = 'Pizza Image';
+img.appendChild(myIcon);
+
+// Desplegar le pagina home
 Home();
 
+// Ponerle las funciones a los botones
 const btnHome = document.querySelector('#home');
 btnHome.addEventListener('click', () => {
     Home();
